6f910c3a87a52eab0a4b53b2a39c7e51b7a04def
Isaac Westwood <roobubba@gmail.com>
2016-04-26 14:58:38 +0000

    start reviews

---
+++ app/models/review.rb
@@ -0,0 +1,12 @@
+class Review < ActiveRecord::Base
+  belongs_to :chef
+  belongs_to :recipe
+
+  validates :chef_id, presence: true
+  validates :recipe_id, presence: true
+  validates :body, presence: true, length: { minimum: 5, maximum: 250 }
+  default_scope -> { order(updated_at: :desc) }
+  
+  validates_uniqueness_of :chef, scope: :recipe
+  
+end
